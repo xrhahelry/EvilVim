a3da1e5a1d5576c0b135831c148ff04876cfafac
XRhahelry <sujalbajracharya1@gmail.com>
2021-09-12 21:04:02 +0545

    change plug-config from folders to files

---
+++ lua/plug-config/nv-bufferline.lua
@@ -0,0 +1,13 @@
+require('bufferline').setup {
+  options = {
+    indicator_icon = '▎',
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
+    left_trunc_marker = '',
+    right_trunc_marker = '',
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    tab_size = 18
+  }
+}
