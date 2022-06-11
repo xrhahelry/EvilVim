dfcd90cef7fa517dd8b2235b061b22d766afa84c
SujalBajra <sujalbajracharya1@gmail.com>
2022-06-11 08:40:17 +0545

    org mode highlighting

--- lua/config/ev-treesitter.lua
+++ lua/config/ev-treesitter.lua
@@ -1,6 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = "python", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"python", "org"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,              -- false will disable the whole extension
+    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
   }
 }
